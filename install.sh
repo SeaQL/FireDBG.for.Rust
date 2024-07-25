bba000a719d64a6047b8a2328ab2bb5651978f50
Billy Chan <ccw.billy.123@gmail.com>
2024-07-25 20:43:00 +0800

    install 1.80.0

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ get_firedbg_version() {
             local _firedbg_version="1.79.0"
             ;;
         rustc\ 1.80.*)
-            local _firedbg_version="1.80.0-beta.1"
+            local _firedbg_version="1.80.0"
             ;;
         *)
             err "no precompiled binaries available for $_rustc_version";
